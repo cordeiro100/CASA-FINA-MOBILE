d5629e24d89c5503e6821857ba9dca09529ea65e
cordeiro100 <mosscordeiro@hotmail.com>
2022-02-14 15:20:55 -0300

    casafina

---
+++ navbar-animation-fix.js
@@ -0,0 +1,8 @@
+$('#bs-example-navbar-collapse-1').on('show.bs.collapse', function() {
+    $('.topcasafina-banner').css('transform', 'translate(-50%, 10%');
+});
+
+
+$('#bs-example-navbar-collapse-1').on('hide.bs.collapse', function() {
+    $('.topcasafina-banner').css('transform', 'translate(-50%, -100%');
+});
